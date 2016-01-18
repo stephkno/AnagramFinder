7da083265acbce9d6692a64aaa5c5061bf6b92e7
stephkno <stephkno@gmail.com>
2016-01-18 00:16:44 -0800

    Update AnagramFinder.py

--- AnagramFinder.py
+++ AnagramFinder.py
@@ -2,44 +2,31 @@ import os
 import re
 import sys
 os.system("clear")
-
 dict = open('/usr/share/dict/words', 'r')
 words = dict.read().splitlines()
-
 wordsout = []
 def permutations(string):
-
 	words = []
 	letters = []
 	suffix = []
 	for letter in string:
 		letters.append(letter)
-	
 	f = 0
-	
 	for i, val in enumerate(letters):
-		
 		suffix = []	
 		prefix = letters[i]
 		for l, val in enumerate(letters):
 			if l != i:
 				suffix.append(letters[l])
-		
 		a = len(suffix)
 		b = len(suffix)-1
 		x = a*b
-
 		for z in range(0, x):
-			
-					
 			out = ""
-			 
 			out += prefix
 			for s in suffix:
 				out += s
-			
 			wordsout.append(out)
-
 			if f < len(suffix)-1:
 				c = suffix[f]
 				suffix[f] = suffix[f+1]
@@ -47,95 +34,13 @@ def permutations(string):
 				f += 1
 			if f >= len(suffix)-1:
 				f = 0
-		
 	return wordsout
-	
 unbuffered = os.fdopen(sys.stdout.fileno(), 'w', 0)
 sys.stdout = unbuffered
 print ("ANAGRAM FINDER V 1.0 COPYLEFT STEPHEN KNOTTS 2016")
 count = 0
 oldline = ""
 for word in words:
-
-	wo = permutations(str(word))
-	count = 0	
-	for line in wo:
-			
-		for w in words:
-			count+=1
-			if line != oldline:
-				print ''
-				print("Trying " + line),
-				line = oldline
-			if count % 20000 < 1:
-				print ".",	
-			if w != word:
-
-				if re.match("^" + w + "$", word):
-					#print
-					print ("FOUND MATCH! "),
-					print w
-6502:permutations wiltors$ sudo vim permutations.py 
-6502:permutations wiltors$ cat permutations.py 
-import os
-import re
-import sys
-os.system("clear")
-
-dict = open('/usr/share/dict/words', 'r')
-words = dict.read().splitlines()
-
-wordsout = []
-def permutations(string):
-
-	words = []
-	letters = []
-	suffix = []
-	for letter in string:
-		letters.append(letter)
-	
-	f = 0
-	
-	for i, val in enumerate(letters):
-		
-		suffix = []	
-		prefix = letters[i]
-		for l, val in enumerate(letters):
-			if l != i:
-				suffix.append(letters[l])
-		
-		a = len(suffix)
-		b = len(suffix)-1
-		x = a*b
-
-		for z in range(0, x):
-			
-					
-			out = ""
-			 
-			out += prefix
-			for s in suffix:
-				out += s
-			
-			wordsout.append(out)
-
-			if f < len(suffix)-1:
-				c = suffix[f]
-				suffix[f] = suffix[f+1]
-				suffix[f+1] = c
-				f += 1
-			if f >= len(suffix)-1:
-				f = 0
-		
-	return wordsout
-	
-unbuffered = os.fdopen(sys.stdout.fileno(), 'w', 0)
-sys.stdout = unbuffered
-print ("ANAGRAM FINDER V 1.0 COPYLEFT STEPHEN KNOTTS 2016")
-count = 0
-oldline = ""
-dude = ["anagrams"]
-for word in dude:
 	print ("Word: " + word),
 	wo = permutations(str(word))
 	wo.pop(0)
@@ -144,7 +49,6 @@ for word in dude:
 	if len(wo) > 0:
 		count = 0	
 		for line in wo:
-			
 			for w in words:
 				count+=1
 				if line != oldline and line != word:
